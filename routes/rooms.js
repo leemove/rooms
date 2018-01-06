e87555bffb12e382542bb14e455ca9d693763bd4
leemove <923522454@qq.com>
2018-01-06 14:56:50 +0800

    取消调试信息

--- routes/rooms.js
+++ routes/rooms.js
@@ -4,6 +4,49 @@ const AV = require('leanengine');
 const Room = AV.Object.extend('Room');
 const _ = require('lodash')
 
+router.get('/', async (ctx, next) => {
+  ctx.state.title = '房源列表'
+  const page = ctx.query.page || 1
+  skip = Math.max(0, (page-1) * 10)
+  const query = new AV.Query('Room')
+  const res = await query.equalTo('sold', false).limit(10).skip(skip).find()
+  const total = await query.count()
+  const rooms = res.map(item => {
+    return {
+      title: item.get('title'),
+      describe: item.get('describe'),
+      price: item.get('price'),
+      wechat: item.get('wechat'),
+      auth: item.get('auth'),
+      id: item.id
+    }
+  })
+  ctx.state.rooms = rooms
+  const totalPage = parseInt(total/10) + 1
+  // console.log(new Array(totalPage))
+  const arr = _.range(totalPage)
+  const pages = arr.map((item, index) => {
+    const currentPage = item + 1
+    return {
+      url: '/rooms?page=' + currentPage,
+      name: currentPage,
+      isActive: page == currentPage
+    }
+  })
+  // console.log(ctx.state)
+  ctx.state.pages = pages
+  await next()
+  await ctx.render('rooms.ejs')
+})
+
+router.get('/add', async (ctx, next) => {
+  ctx.state = {
+    title: '新增房源'
+  }
+  await next()
+  await ctx.render('addRoom.ejs', {})
+})
+
 router.post('/', async (ctx, next) => {
   const body = ctx.request.body
   if (body.power !== 'maymay') {
@@ -42,50 +85,7 @@ router.post('/', async (ctx, next) => {
   ctx.redirect('/rooms')
 })
 
-router.get('/', async (ctx, next) => {
-  ctx.state.title = '房源列表'
-  const page = ctx.query.page || 1
-  skip = Math.max(0, (page-1) * 10)
-  const query = new AV.Query('Room')
-  const res = await query.equalTo('sold', false).limit(10).skip(skip).find()
-  const total = await query.count()
-  const rooms = res.map(item => {
-    return {
-      title: item.get('title'),
-      describe: item.get('describe'),
-      price: item.get('price'),
-      wechat: item.get('wechat'),
-      auth: item.get('auth'),
-      id: item.id
-    }
-  })
-  ctx.state.rooms = rooms
-  const totalPage = parseInt(total/10) + 1
-  // console.log(new Array(totalPage))
-  const arr = _.range(totalPage)
-  const pages = arr.map((item, index) => {
-    const currentPage = item + 1
-    console.log(page, currentPage)
-    return {
-      url: '/rooms?page=' + currentPage,
-      name: currentPage,
-      isActive: page == currentPage
-    }
-  })
-  console.log(rooms)
-  // console.log(ctx.state)
-  ctx.state.pages = pages
-  await next()
-  await ctx.render('rooms.ejs')
-})
 
-router.get('/add', async (ctx, next) => {
-  ctx.state = {
-    title: '新增房源'
-  }
-  await next()
-  await ctx.render('addRoom.ejs', {})
-})
 
 router.get('/detail/:id', async (ctx, next) => {
   ctx.state.title = '测试页面'
--- routes/todos.js
+++ routes/todos.js
@@ -2,9 +2,7 @@
 
 const AV = require('leanengine');
 const Router = require('koa-router');
-
 const router = new Router({prefix: '/todos'});
-
 const Todo = AV.Object.extend('Todo');
 
 // 查询 Todo 列表
@@ -16,8 +14,6 @@ router.get('/', async function(ctx) {
     ctx.state.todos = await query.find();
   } catch (err) {
     if (err.code === 101) {
-      // 该错误的信息为：{ code: 101, message: 'Class or object doesn\'t exists.' }，说明 Todo 数据表还未创建，所以返回空的 Todo 列表。
-      // 具体的错误代码详见：https://leancloud.cn/docs/error_code.html
       ctx.state.todos = [];
     } else {
       throw err;
